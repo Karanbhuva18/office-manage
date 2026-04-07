838dcd7b73a4bf8a67980a6c5b7519c9fffa78a9
Karan bhuva <karanbhuva9861@gmail.com>
2026-04-08 01:37:37 +0530

    client module complete

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -7,7 +7,7 @@ export const createClient = async (req, res) => {
       return res.status(400).json({ message: "required fileds are empty" });
     }
 
-    const existingClient = await Client.findOne({ email });
+    const existingClient = await Client.findOne({ where: { email } });
     if (existingClient) {
       return res.status(400).json({ message: "client already exist" });
     }
@@ -18,5 +18,88 @@ export const createClient = async (req, res) => {
       .json({ message: "client created sucessfully", data: createdClient });
   } catch (error) {
     return res.status(500).json({ error: MessageChannel.error });
+  }
+};
+
+export const showClients = async (req, res) => {
+  try {
+    let { page = 1, limit = 10, client } = req.query;
+
+    page = parseInt(page);
+    limit = parseInt(limit);
+
+    let whereClause = {};
+
+    if (client) {
+      whereClause.name = {
+        [Op.like]: `%${client}%`,
+      };
+    }
+
+    const offset = (page - 1) * limit;
+
+    const { count, rows } = await Client.findAndCountAll({
+      where: whereClause,
+      limit,
+      offset,
+      order: [["createdAt", "DESC"]],
+    });
+
+    const totalPage = Math.ceil(count / limit);
+    const hasNextPage = page < totalPage;
+    const hasPrevPage = page > 1;
+
+    return res.status(200).json({
+      totalItems: count,
+      totalPage,
+      currentPage: page,
+      hasNextPage,
+      hasPrevPage,
+      data: rows,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateClients = async (req, res) => {
+  try {
+    const { name, email, phone } = req.body;
+    const id = req.params.id;
+    const client = await Client.findOne({
+      where: { id },
+    });
+    if (!client) {
+      return res.status(404).json({ message: "Client not found" });
+    }
+    await client.update({ name, email, phone });
+
+    return res.status(200).json({ message: "client Updated", data: client });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteClients = async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (!id) {
+      return res.status(400).json({ message: "id not found" });
+    }
+    const deletedRows = await Client.destroy({
+      where: { id },
+    });
+
+    if (deletedRows === 0) {
+      return res.status(404).json({
+        message: "Client not found",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Client deleted successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
 };
--- routes/client.routes.js
+++ routes/client.routes.js
@@ -1,9 +1,12 @@
-import { createClient } from "../controllers/client.controller.js";
+import { createClient, deleteClients, showClients, updateClients } from "../controllers/client.controller.js";
 import express from "express";
 import { userLoginCheck, userRole } from "../middleware/auth.middleware.js";
 
 const routes = express.Router();
 
-routes.post("/createClient",userLoginCheck,userRole(['admin']), createClient);
+routes.post("/createClient", userLoginCheck, userRole(["admin"]), createClient);
+routes.get("/getClient", userLoginCheck, showClients);
+routes.put("/update/:id", userLoginCheck, updateClients);
+routes.delete("/delete/:id",userLoginCheck,deleteClients);
 
 export default routes;
