c038da4349838a85d536620f2c5c73ecf63ff2e8
Karan bhuva <karanbhuva9861@gmail.com>
2026-06-29 22:24:50 +0530

    fix: correct database name typo in Sequelize initialization

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 import Sequelize from "sequelize";
 
-const sequlize = new Sequlize("office-manage", "root", "", {
+const sequlize = new Sequlize("office_manage", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
