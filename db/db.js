0be971c6d49d9fea77877a2262ff57874f8daaa3
Karan bhuva <karanbhuva9861@gmail.com>
2026-06-29 22:29:18 +0530

    fix: update Sequelize initialization with correct username and password

--- db/db.js
+++ db/db.js
@@ -1,8 +1,13 @@
 import Sequelize from "sequelize";
 
-const sequelize = new Sequelize("office_manage", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  "office_manage",
+  "office_user",
+  "office123",
+  {
+    host: "localhost",
+    dialect: "mysql",
+  }
+);
 
 export default sequelize;
